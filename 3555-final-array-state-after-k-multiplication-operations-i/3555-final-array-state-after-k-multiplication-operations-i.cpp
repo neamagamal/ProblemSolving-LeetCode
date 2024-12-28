9d0c53822d4574dbae496418fbad1a35dd69b27a
neama gamal <126347630+neamagamal@users.noreply.github.com>
2024-12-28 19:34:24 +0200

    Time: 0 ms (100%), Space: 27 MB (92.17%) - LeetHub

---
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) 
+    {
+        while (k--){
+          int min =nums[0];
+          int minIndex = 0;
+          for (int j =0; j<nums.size(); j++)
+          {
+             if (nums[j] < min){
+              min = nums[j]; 
+                minIndex = j;
+
+             }
+          }
+          nums[minIndex] *= multiplier;
+         
+        }
+      
+     return nums; 
+        
+    }
+};
